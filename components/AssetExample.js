00acc7e838c5ad5c7b137001b127cc7124d22db9
HanyBin <51113403+HanyBin@users.noreply.github.com>
2022-03-25 12:44:39 +0300

    Create AssetExample.js

---
+++ components/AssetExample.js
@@ -0,0 +1,26 @@
+s (20 sloc)  463 Bytes
+   
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+   
+      <Image style={styles.logo} source={require('../assets/cat.jpg')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  
+  logo: {
+    height: 228,
+    width: 228,
+  }
+});
